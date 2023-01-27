f111b10c3b89888aacd69350954ab9982ca32ad4
TheMadScripter <123608422+TheMadScripter@users.noreply.github.com>
2023-01-27 14:23:44 +0900

    Initial Creation

---
+++ DAT_Editor.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Copies headers of file
+head -n 1 ./test.dat > ./test_modified.dat
+
+# Counter variable
+count=0
+
+# Use awk to add 0.5 to the second field and print the modified line
+cat ./test.dat | while read -r line; do
+    # Only modify the first 48 lines
+    if [ $count -lt 48 ]; then
+        echo "$line" | awk '{$2+=0.5; print $0}' >> ./test_modified.dat
+        count=$((count+1))
+    else
+        echo "$line" >> ./test_modified.dat
+    fi
+done
