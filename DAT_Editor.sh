e9f6d84b8bf8af174444799c66738e14e68af4cf
TheMadScripter <123608422+TheMadScripter@users.noreply.github.com>
2023-01-29 11:51:30 +0900

    Optimized

--- DAT_Editor.sh
+++ DAT_Editor.sh
@@ -1,29 +1,21 @@
 #!/bin/bash
 
-# Copies header to final file
+# Copies headers of file
 head -n 1 ./test1.pdb > ./test1_mod.pdb
 
-# Counter Variable
+# Counter variable
 count=0
 
-> ./temp_file.txt
-
-# Copies lines 2 through 49 to temp file
+# Use awk to add 0.5 to the second field and print the modified line
 tail -n +2 ./test1.pdb | while read -r line; do
-        # Only modify the first 48 lines
-        if [ $count -lt 48 ]; then
-                echo "$line" >> ./temp_file.txt
-                count=$((count+1))
-        fi
-done
-
-# makes changes to second column number
-cat ./temp_file.txt | while read -r line; do
+    # Only modify the first 48 lines
+    if [ $count -lt 48 ]; then
         temp=$(echo $line | awk '{print $2}')
         total=$(bc -l <<< 'scale=2; '$temp'+.5')
         gtotal=$(echo $total)
         echo -e "$line" | awk -v orig="$temp" -v new="$gtotal" '{gsub(orig, new); print}' >> ./test1_mod.pdb
+        count=$((count+1))
+    else
+        echo "$line" >> ./test1_mod.pdb
+    fi
 done
-
-#copies rest of data from line 50 to new file
-tail -n +50 ./test1.pdb >> ./test1_mod.pdb
